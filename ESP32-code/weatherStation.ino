201dc7beaa82e1a7c53cac24af7f02f491bee80e
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-10-27 16:27:17 +0200

    Update weatherStation.ino

--- ESP32-code/weatherStation.ino
+++ ESP32-code/weatherStation.ino
@@ -1,4 +1,12 @@
 #include "weatherAPI.h"
+#include "weatherLocal.h"
+
+#define DHTTYPE DHT22
+#define DHTPIN 22
+
+WeatherLocal local(DHTPIN, DHTTYPE);
+WeatherAPI api();
+
 
 void setup() {
   Serial.begin(9600);
