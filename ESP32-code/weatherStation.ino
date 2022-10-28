10fbd7c36244b91b99e2fe19d4670737e464955f
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-10-28 14:56:33 +0200

    Update weatherStation.ino

--- ESP32-code/weatherStation.ino
+++ ESP32-code/weatherStation.ino
@@ -1,8 +1,12 @@
+#include <WiFi.h>
 #include "weatherAPI.h"
 #include "weatherLocal.h"
 
 #define DHTTYPE DHT22
 #define DHTPIN 22
+
+const char* ssid = "SSID";
+const char* password = "password";
 
 WeatherLocal local(DHTPIN, DHTTYPE);
 WeatherAPI api();
@@ -10,6 +14,17 @@ WeatherAPI api();
 
 void setup() {
   Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
   
 }
 
