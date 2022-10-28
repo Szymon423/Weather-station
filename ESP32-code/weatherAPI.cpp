f30b121d6781747273eb1e921af3f9add14e41e2
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-10-28 14:44:30 +0200

    update

--- ESP32-code/weatherAPI.cpp
+++ ESP32-code/weatherAPI.cpp
@@ -1,41 +1,79 @@
-#include "weatherAPI.h"
+#include "WeatherAPI.h"
 #include <String.h>
 
-weatherAPI::weatherAPI() {
-  weatherAPI::icon = "";
-  weatherAPI::windSpeed = 0.0;
-  weatherAPI::temp = 0.0;
-  weatherAPI::tempMax = 0.0;
-  weatherAPI::tempMin = 0.0;
-  weatherAPI::tempFeelsLike = 0.0;
-  weatherAPI::pressure = 0;
-  weatherAPI::humidity = 0;
-  weatherAPI::clouds = 0;
-  weatherAPI::APIKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-  weatherAPI::coordinatesLon = "17.0549";
-  weatherAPI::coordinatesLat = "51.1107";
-  weatherAPI::JSON = "";
+WeatherAPI::WeatherAPI() {
+  WeatherAPI::icon = "";
+  WeatherAPI::windSpeed = 0.0;
+  WeatherAPI::temp = 0.0;
+  WeatherAPI::tempMax = 0.0;
+  WeatherAPI::tempMin = 0.0;
+  WeatherAPI::tempFeelsLike = 0.0;
+  WeatherAPI::pressure = 0;
+  WeatherAPI::humidity = 0;
+  WeatherAPI::APIKey = "1c468005da0a50bd2dd88333e3d66285"; // 1c468005da0a50bd2dd88333e3d66285
+  WeatherAPI::coordinatesLon = "17.0549";
+  WeatherAPI::coordinatesLat = "51.1107";
+  WeatherAPI::JSON = "";
+  WeatherAPI::requestString = "";
 }
 
-weatherAPI::~weatherAPI() {
+WeatherAPI::~WeatherAPI() {
   Serial.println("Deleting object...");
 }
 
-void weatherAPI::getData() {
+void WeatherAPI::getData() {
   // send API request and store it at proper file
+
+  return;
 }
 
-void weatherAPI::processData() {
+void WeatherAPI::processData() {
+  // so the response looks like this:
+  /*
+  {"coord":{"lon":17.0549,"lat":51.1107},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations",
+  "main":{"temp":296.59,"feels_like":296.37,"temp_min":295.15,"temp_max":297.03,"pressure":1027,"humidity":53},"visibility":10000,
+  "wind":{"speed":3.6,"deg":220},"clouds":{"all":0},"dt":1666958079,"sys":{"type":2,"id":2073402,"country":"PL","sunrise":1666935408,
+  "sunset":1666971249},"timezone":7200,"id":3081368,"name":"Wrocław","cod":200}
+  */
   // process JSON to separate 
+  WeatherAPI::icon = WeatherAPI::getValueOf("icon");
+  WeatherAPI::windSpeed = WeatherAPI::getValueOf("wind").toInt();
+  WeatherAPI::pressure = WeatherAPI::getValueOf("pressure").toInt();
+  WeatherAPI::humidity = WeatherAPI::getValueOf("humidity").toInt();
+  WeatherAPI::temp = WeatherAPI::getValueOf("temp").toFloat(); 
+  WeatherAPI::tempMax = WeatherAPI::getValueOf("temp_max").toFloat();
+  WeatherAPI::tempMin = WeatherAPI::getValueOf("temp_min").toFloat();
+  WeatherAPI::tempFeelsLike = WeatherAPI::getValueOf("feels_like").toFloat();
+  return;
 }
 
-void weatherAPI::update(){
+void WeatherAPI::update() {
+  // at the very beggining we need to generate request basing on our data
+  WeatherAPI::generateRequestString();
   // first step is to download data from API
-  weatherAPI::getData();
+  WeatherAPI::getData();
   // second step is to process data from JSON to variables
-  weatherAPI::processData();
+  WeatherAPI::processData();
+  return;
 }
 
+void WeatherAPI::generateRequestString() {
+  // example:
+  // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+  WeatherAPI::requestString += "https://api.openweathermap.org/data/2.5/weather?lat=" + String(WeatherAPI::coordinatesLat); 
+  WeatherAPI::requestString += "&lon=" + String(WeatherAPI::coordinatesLon) + "&appid=" + String(WeatherAPI::APIKey);
+  return;
+}
+
+String WeatherAPI::getValueOf(String str) {
+  // find start string
+  int startPos = WeatherAPI::JSON.indexOf(str) + str.length() + 2;
+  // find end char position basing on input type
+  int finishPos = (str.equals("icon") ? WeatherAPI::JSON.indexOf('"', startPos) : WeatherAPI::JSON.indexOf('"', startPos));
+  // return proper string
+  return WeatherAPI::JSON.substring(startPos, finishPos);
+}
 
 
 
